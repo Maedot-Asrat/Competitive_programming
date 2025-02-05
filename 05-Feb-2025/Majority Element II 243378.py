c1dade30c170e92ac740e30d2652b04280349082
Maedot Asrat <147245322+Maedot-Asrat@users.noreply.github.com>
2025-02-05 16:13:42 +0300

    Created 'Majority Element II' by A2SV Hub

---
+++ 05-Feb-2025/Majority Element II 243378.py
@@ -0,0 +1,9 @@
+# Problem: Majority Element II - https://leetcode.com/problems/majority-element-ii/?envType=daily-question&envId=2023-10-05
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        ans=[]
+        for key,value in Counter(nums).items():
+            if value > floor(len(nums)/3):
+                ans.append(key)
+        return ans
