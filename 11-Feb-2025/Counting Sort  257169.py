bf73a3b6582beb8c554ab9e5915670927c32d7f8
Maedot Asrat <147245322+Maedot-Asrat@users.noreply.github.com>
2025-02-11 16:15:43 +0300

    Created 'Counting Sort ' by A2SV Hub

---
+++ 11-Feb-2025/Counting Sort  257169.py
@@ -0,0 +1,37 @@
+# Problem: Counting Sort  - https://www.hackerrank.com/challenges/countingsort1/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countingSort' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def countingSort(arr):
+   max_num= 100
+   new_arr=[0]*max_num
+   for num in arr:
+    new_arr[num]+=1
+   return new_arr
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = countingSort(arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
