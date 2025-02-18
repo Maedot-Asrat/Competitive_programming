cfb1b6a8296294623cf40761d132a1d155c384cc
Maedot Asrat <147245322+Maedot-Asrat@users.noreply.github.com>
2025-02-18 15:47:40 +0300

    Created 'D - Bomb Detection Validation' by A2SV Hub

---
+++ A2SV G6 - Round #2 18-Feb-2025/D - Bomb Detection Validation 271751.py
@@ -0,0 +1,35 @@
+# Problem: D - Bomb Detection Validation - https://codeforces.com/gym/586960/problem/D
+
+class solution:
+    def bomb_validation():
+        n,m=input().split()
+        matrix=[[0]*int(m) for _ in range(int(n))]
+        for i in range(int(n)):
+            accept=list(input())
+            matrix[i]=accept
+        flag="YES"
+        for i in range(int(n)):
+             for j in range(int(m)):
+                count=0
+                if i<int(n)-1 and matrix[i+1][j]=="*":
+                    count+=1
+                if j<int(m)-1 and matrix[i][j+1]=="*":
+                    count+=1
+                if i<int(n)-1 and j<int(m)-1 and matrix[i+1][j+1]=="*":
+                    count+=1
+                if i!=0 and matrix[i-1][j]=="*":
+                    count+=1
+                if j!=0 and matrix[i][j-1]=="*":
+                    count+=1
+                if i!=0 and j!=0 and matrix[i-1][j-1]=="*":
+                    count+=1
+                if i!=0 and j<int(m)-1 and matrix[i-1][j+1]=="*":
+                    count+=1
+                if i<int(n)-1 and j!=0 and matrix[i+1][j-1]=="*":
+                    count+=1
+                if matrix[i][j]=="." and count!=0:
+                    flag="NO"
+                if matrix[i][j]!="*" and matrix[i][j]!="." and int(matrix[i][j])!=count:
+                    flag="NO"
+        print(flag)
+    bomb_validation()
