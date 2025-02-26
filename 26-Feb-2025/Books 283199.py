df36bef92cb10b0c1d90ceb7cada2eb389716ee4
Maedot Asrat <147245322+Maedot-Asrat@users.noreply.github.com>
2025-02-26 13:05:00 +0300

    Created 'Books' by A2SV Hub

---
+++ 26-Feb-2025/Books 283199.py
@@ -0,0 +1,18 @@
+# Problem: Books - https://codeforces.com/contest/279/problem/B
+
+n,t=(map(int, input().split()))
+arr=list(map(int, input().split()))
+start=0
+end=0
+summ=0
+ans=0
+while end<len(arr) and start<len(arr):
+    summ+=arr[end]
+    if summ<=t:
+        ans=max(ans,end-start+1)
+    else:
+        summ-=arr[start]
+        start+=1
+    end+=1
+print(ans)
+        
