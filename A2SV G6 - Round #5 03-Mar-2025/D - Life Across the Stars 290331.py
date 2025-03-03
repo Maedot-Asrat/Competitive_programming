71ebac66bb99be1d5113bf15fb33f74ff1382275
Maedot Asrat <147245322+Maedot-Asrat@users.noreply.github.com>
2025-03-04 00:26:08 +0300

    Created 'D - Life Across the Stars' by A2SV Hub

---
+++ A2SV G6 - Round #5 03-Mar-2025/D - Life Across the Stars 290331.py
@@ -0,0 +1,23 @@
+# Problem: D - Life Across the Stars - https://codeforces.com/gym/591928/problem/D
+
+from collections import defaultdict, Counter
+t= int(input())
+track=defaultdict(int)
+for _ in range(t):
+    b,d=map(int, input().split())
+    track[b]+=1
+    track[d]-=1
+track=dict(sorted(track.items()))
+years=list(track.keys())
+pref_sum=[0]+ list(track.values())
+for i in range(1,len(pref_sum)):
+    pref_sum[i]+=pref_sum[i-1]
+maxi=max(pref_sum)
+year=0
+for j in range(len(track)):
+    if pref_sum[j]==maxi:
+        year=years[j-1]
+        break
+print(year, maxi)
+
+
