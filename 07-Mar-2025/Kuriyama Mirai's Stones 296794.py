88b877ee7517450fa785ae040d7aa11b4999e234
Maedot Asrat <147245322+Maedot-Asrat@users.noreply.github.com>
2025-03-07 17:48:39 +0300

    Created 'Kuriyama Mirai's Stones' by A2SV Hub

---
+++ 07-Mar-2025/Kuriyama Mirai's Stones 296794.py
@@ -0,0 +1,22 @@
+# Problem: Kuriyama Mirai's Stones - https://codeforces.com/contest/433/problem/B
+
+t=int(input())
+costs= list(map(int, input().split()))
+arranged_costs=sorted(costs)
+n=int(input())
+for i in range(1,t):
+    costs[i]+=costs[i-1]
+for i in range(1,t):
+    arranged_costs[i]+=arranged_costs[i-1]
+for _ in range(n):
+    type, l, r =map(int, input().split())
+    if type==1:
+        if l!=1:
+            print(costs[r-1]-costs[l-2])
+        else:
+            print(costs[r-1])
+    else:
+        if l!=1:
+            print(arranged_costs[r-1]-arranged_costs[l-2])
+        else:
+             print(arranged_costs[r-1])
