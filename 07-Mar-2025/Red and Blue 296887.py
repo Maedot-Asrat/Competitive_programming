56d3986adddd36f856a3278db69a5cebb2dc3efb
Maedot Asrat <147245322+Maedot-Asrat@users.noreply.github.com>
2025-03-07 18:11:29 +0300

    Created 'Red and Blue' by A2SV Hub

---
+++ 07-Mar-2025/Red and Blue 296887.py
@@ -0,0 +1,20 @@
+# Problem: Red and Blue - https://codeforces.com/contest/1469/problem/B
+
+t = int(input())
+for _ in range(t):
+    r=int(input())
+    red=list(map(int, input().split()))
+    b=int(input())
+    blue=list(map(int, input().split()))
+    for i in range(1,r):
+        red[i]+=red[i-1]
+    for j in range(1,b):
+        blue[j]+=blue[j-1]
+    maxi=0
+    redd=max(red)
+    blued=max(blue)
+    if redd>0:
+        maxi+=redd
+    if blued>0:
+        maxi+=blued
+    print(maxi)
