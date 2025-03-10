3fcd3c46620e253a3b1c3a34ded0499f42feccdf
Maedot Asrat <147245322+Maedot-Asrat@users.noreply.github.com>
2025-03-10 19:33:06 +0300

    Created 'C - Minimal TV Subscriptions' by A2SV Hub

---
+++ A2SV G6 - Round #3 10-Mar-2025/C - Minimal TV Subscriptions 300529.py
@@ -0,0 +1,27 @@
+# Problem: C - Minimal TV Subscriptions - https://codeforces.com/gym/588468/problem/C
+
+from collections import Counter
+def check(shows,n,d):
+    left=0
+    right=d
+    track=Counter(shows[left:right])
+    window=len(track)
+    if len(shows)==d:
+        return window
+    while right<n:
+        if shows[right]!= shows[left]:
+            track[shows[left]] -= 1
+        if track[shows[left]] == 0:
+            del track[shows[left]]
+        if shows[right]!=shows[left]:
+            track[shows[right]] += 1
+        window = min(window, len(track))
+        left += 1
+        right += 1
+    
+    return window
+t=int(input())
+for _ in range(t):
+    n,k,d=map(int, input().split())
+    shows=list(map(int,input().split()))
+    print(check(shows,n,d))
