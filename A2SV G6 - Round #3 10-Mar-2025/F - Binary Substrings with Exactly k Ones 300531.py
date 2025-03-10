f327bedf5c4d5a74d3225741b530022bd2810109
Maedot Asrat <147245322+Maedot-Asrat@users.noreply.github.com>
2025-03-10 19:35:19 +0300

    Created 'F - Binary Substrings with Exactly k Ones' by A2SV Hub

---
+++ A2SV G6 - Round #3 10-Mar-2025/F - Binary Substrings with Exactly k Ones 300531.py
@@ -0,0 +1,22 @@
+# Problem: F - Binary Substrings with Exactly k Ones - https://codeforces.com/gym/588468/problem/F
+
+k = int(input())
+nums = input()
+n = len(nums)
+def mostK(k):
+    if k == -1:
+        return 0
+    left = 0 
+    cnt = 0
+    ans = 0
+    for right in range(n):
+        if nums[right] == "1":
+            cnt += 1  
+        while cnt > k:
+            if nums[left] == "1":
+                cnt -= 1  
+            left += 1 
+        ans += right - left + 1
+    
+    return ans 
+print(mostK(k) - mostK(k-1)) 
