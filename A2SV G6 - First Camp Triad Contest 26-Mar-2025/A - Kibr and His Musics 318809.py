bf8012e9f831c7b64e625b842747e56174d90e84
Maedot Asrat <147245322+Maedot-Asrat@users.noreply.github.com>
2025-03-26 17:35:55 +0300

    Created 'A - Kibr and His Musics' by A2SV Hub

---
+++ A2SV G6 - First Camp Triad Contest 26-Mar-2025/A - Kibr and His Musics 318809.py
@@ -0,0 +1,15 @@
+# Problem: A - Kibr and His Musics - https://codeforces.com/gym/589822/problem/A
+
+n, m = map(int, input().split())
+prefix = []
+for i in range(n):
+    c, t = map(int, input().split())
+    prefix.append(c * t)
+    if i > 0:
+        prefix[i] += prefix[i - 1]
+moments = list(map(int, input().split()))
+i = 0
+for j in range(m):
+    while prefix[i] < moments[j]:
+        i += 1
+    print(i + 1)
