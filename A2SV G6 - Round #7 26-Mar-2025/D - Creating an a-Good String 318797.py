27a9298d25ba9f6afc44184fef25d8bc90111c60
Maedot Asrat <147245322+Maedot-Asrat@users.noreply.github.com>
2025-03-26 17:33:07 +0300

    Created 'D - Creating an a-Good String' by A2SV Hub

---
+++ A2SV G6 - Round #7 26-Mar-2025/D - Creating an a-Good String 318797.py
@@ -0,0 +1,23 @@
+# Problem: D - Creating an a-Good String - https://codeforces.com/gym/596141/problem/D
+
+def calc(l, r, c):
+    if l == r:
+        if s[l] == c:
+            return 0
+        return 1
+    
+    mid = (l + r) // 2
+    
+    left_changes = (mid - l + 1) - s[l:mid + 1].count(c)    
+    left_result = left_changes + calc(mid + 1, r, chr(ord(c) + 1))
+ 
+    right_changes = (r - mid) - s[mid + 1:r + 1].count(c)
+    right_result = right_changes + calc(l, mid, chr(ord(c) + 1))
+ 
+    return min(left_result, right_result)
+ 
+for _ in range(int(input())):
+    n = int(input())
+    s = input()
+ 
+    print(calc(0, len(s) - 1, 'a'))
